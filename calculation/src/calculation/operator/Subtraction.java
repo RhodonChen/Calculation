86e2a9bd2e9daa2d99572864b0be13ad440fef1a
Chen Xiaoyan <Chen Xiaoyan@LAPTOP-FMNTU266.eduroam.wireless.dtu.dk>
2018-03-21 09:37:37 +0100

    subtraion added

---
+++ calculation/src/calculation/operator/Subtraction.java
@@ -0,0 +1,20 @@
+package calculation.operator;
+
+public class Subtraction extends Operation {
+
+	@Override
+	public String getName() {
+		return "subtraction";
+	}
+
+	@Override
+	public String getOperator() {
+		return "-";
+	}
+
+	@Override
+	public double calc() {
+		return a - b;
+	}
+
+}
